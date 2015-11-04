928c1ae62092fc4433b880d0cedb0392d4508421
styuan <893292728@qq.com>
2015-11-04 13:27:03 +0800

    Create Task.java

--- Task.java
+++ Task.java
@@ -1,1 +1,41 @@
 
+/**  
+ * @ClassName     Task.java  
+ * @Description   task数据类
+ * @author        yuanshengtao 
+ * @version       V1.0    
+ * @Date          2015-11-02  
+ */ 
+
+package ymlfileparser;
+
+class Task {
+	private String executable;     //executable 属性
+	private boolean failonerror;   //failonerror 属性
+	private String type;	       //type属性
+	
+	public void setExecutable(String ConfigExecutable){
+		executable = ConfigExecutable;
+	}
+	
+	public void setFailonerror(boolean ConfigFailOnError){
+		failonerror = ConfigFailOnError;
+	}
+	
+	public void setType(String ConfigType){
+		type = ConfigType;
+	}
+	
+	public String getExecutable(){
+		return executable;
+	}
+	
+	public boolean getFailonerror(){
+		return failonerror;
+	}
+	
+	public String getType(){
+		return type;
+	}
+	
+}
